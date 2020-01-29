eb1b700297e0d5e64fe4348a0d354489fd6b05bf
Fatma Dogan and Joseph Wangemann <fatma+joseph@>
2020-01-29 11:01:05 -0800

    add random number generator function

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,40 @@ function Player(name) {
     this.score = 0;
 }
 
+Player.prototype.updateScore = function() {
+    this.score += tempScore
+}
 
-
+function getNumber() {
+   return Math.floor(Math.random() * 6 + 1);
+}
 
 
 
 //user interface
 var player1 = "";
 var player2 = "";
+var tempScore = 0;
 
 $(document).ready(function() {
+   //click function for start screen
     $("form").submit(function() {
         event.preventDefault();
         player1 = new Player($("input#player1").val());
         player2 = new Player($("input#player2").val());
         $("#start-screen").hide();
         $("#play-screen").show();
-        console.log(player2.name);
-        console.log(player1.name);
     });
-
+    //click function for roll
+    $("#roll").click(function() {
+        var currentRoll = getNumber();
+        console.log(currentRoll);
+        // if ( currentRoll == 1 ) {
+        //     //switch the player
+        //     tempScore = 0;
+        // } else {
+        //    return tempScore += currentRoll;
+        // }
+    });
+    //click function for hold
 });
