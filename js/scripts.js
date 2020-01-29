8829f28d37978a41d989710652d6842b8a5017c6
Fatma Dogan and Joseph Wangemann <fatma+joseph@>
2020-01-29 10:28:36 -0800

    add click function to submit player names and switch from start screen to play screen

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,21 @@ function Player(name) {
 
 
 
-//user interface
-$(document).ready(function() {
 
+
+//user interface
+var player1 = "";
+var player2 = "";
+
+$(document).ready(function() {
+    $("form").submit(function() {
+        event.preventDefault();
+        player1 = new Player($("input#player1").val());
+        player2 = new Player($("input#player2").val());
+        $("#start-screen").hide();
+        $("#play-screen").show();
+        console.log(player2.name);
+        console.log(player1.name);
+    });
 
 });
